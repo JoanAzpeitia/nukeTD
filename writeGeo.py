8494bc641652fe9ca1dc526fad1c37fead507c96
JoanAzpeitia <33152421+JoanAzpeitia@users.noreply.github.com>
2021-09-11 12:59:25 +0200

    Add files via upload

---
+++ writeGeo.py
@@ -0,0 +1,37 @@
+import nuke
+import os
+
+def writeGeoABC():
+    # find shot path
+    scriptPath = nuke.root().name()
+    relativePath = os.path.split(scriptPath)[0]
+    elementsDir = os.path.split(relativePath)[0] + '/elements/nukeExports/'
+
+    writeGeoNode = nuke.createNode('WriteGeo')
+    # create name
+    name = writeGeoNode['name'].value()
+    namePath = name + 'NukeAlembicExport'
+    # check ext
+    ext = writeGeoNode['file_type'].setValue('abc')
+    extPath = 'abc'
+    # build Write Geo path
+    geoPath = elementsDir + namePath + '.' + extPath
+    writeGeoNode.knob('file').setValue(geoPath)
+
+
+def writeGeoFBX():
+    # find shot path
+    scriptPath = nuke.root().name()
+    relativePath = os.path.split(scriptPath)[0]
+    elementsDir = os.path.split(relativePath)[0] + '/elements/nukeExports/'
+
+    writeGeoNode = nuke.createNode('WriteGeo')
+    # create name
+    name = writeGeoNode['name'].value()
+    namePath = name + 'nukeFBXExport'
+    # check ext
+    ext = writeGeoNode['file_type'].setValue('fbx')
+    extPath = 'fbx'
+    # build Write Geo path
+    geoPath = elementsDir + namePath + '.' + extPath
+    writeGeoNode.knob('file').setValue(geoPath)
